d01255b2f1c1e23366127ab44a2e15bdda0b8742
YusifQasim <yusifqasim22@gmail.com>
2023-07-26 04:14:58 +0300

    xl size designed

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="h-screen" id="">
-      <div>
-        <div>
+    // intire screen dev
+    <div className="h-screen flex justify-center items-center relative" id="">
+      {/* linkes dev */}
+      <div className="flex justify-center items-center gap-8 mb-24 flex-wrap ">
+        <div className="flex flex-col gap-6 ">
           <Image
             src="/linked.svg"
             alt="linked in logo"
@@ -27,25 +29,30 @@ const Home = () => {
             priority
           />
         </div>
-
-        <div>
+        {/* the texts and the scroll down div */}
+        <div className=" w-3/4 md:w-1/3  order-3 md:order-none">
           <div className="">
             <div>
-              {" "}
-              <h1 className="">Hi, I am Yusif</h1>
-              <h3 className="">
-                Front End Web Developer & Spring Boot Developer
+              <h1 className="text-5xl text-white font-semibold break-words w-52 leading-tight">Hi, I am Yusif</h1>
+              <h3 className=" text-base font-bold text-grayColor  mb-3">
+                Front End Web Developer 
               </h3>
-              <p className="">
+              <p className="w-4/4 text-xs text-grayColor font-medium mb-3">
                 I have a passion for building intuitive, user-friendly
                 interfaces that provide an enjoyable and seamless user
                 experience.
               </p>
-              <a href="#contact">
-                Contact Me<i className=""></i>
+              <a href="#contact" className="flex gap-2 bg-textColor text-white rounded-lg w-36 p-2 mt-4">
+                Contact Me  <Image
+            src="/direct.svg"
+            alt="Next.js Logo"
+            width={18}
+            height={18}
+            priority
+          />
               </a>
             </div>
-            <div>
+            <div className="flex gap-1 absolute bottom-32 ">
               <Image
                 src="/mouse.svg"
                 alt="mouse Logo"
@@ -55,6 +62,7 @@ const Home = () => {
               />
               <p>Scroll Down</p>
               <Image
+            
                 src="/down.svg"
                 alt="down Logo"
                 width={24}
@@ -64,15 +72,15 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div>
-        <Image
-            src="/telegram.svg"
+        {/* personal image div */}
+        <div className="">
+          <Image
+            src="/my.jpg"
             alt="Next.js Logo"
-            width={24}
-            height={24}
+            width={270}
+            height={270}
             priority
           />
-
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,12 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        textColor:'	#6e57e0'
+        textColor:'	#6e57e0',
+        grayColor:'#bcbac4'
       },
     
     },
   },
   plugins: [],
+  
 }
