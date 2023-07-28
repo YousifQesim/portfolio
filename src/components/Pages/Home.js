f95279b9b5f89f3e5c7078678a5e6f2dd4e46e37
YusifQasim <yusifqasim22@gmail.com>
2023-07-28 16:54:54 +0300

    Qualification buttons done

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+
 import Link from "next/link";
 
 const AnimatedImage = ({ src, alt, width, height }) => {
--- src/components/Pages/Qualification.js
+++ src/components/Pages/Qualification.js
@@ -1,4 +1,5 @@
 "use client"
+import { motion } from "framer-motion";
 import Head from 'next/head';
 import { useState } from 'react';
 import Link from 'next/link';
@@ -6,62 +7,100 @@ import educationData from '/education.json';
 import workData from '/work.json';
 import certificationData from '/certification.json';
 import Timeline from '../timeline';
+import Image from "next/image";
 
 export default function Qualification() {
   const [showEducationTimeline, setShowEducationTimeline] = useState(false);
   const [showWorkTimeline, setShowWorkTimeline] = useState(true);
   const [showCertificationTimeline, setShowCertificationTimeline] = useState(false);
+  const [activeButton, setActiveButton] = useState('education');
 
   return (
     <div>
-      <Head>
-        <title>My Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="text-3xl font-bold mb-8">My Portfolio</h1>
-        <ul className="list-disc list-inside">
-          <li>
-            <button
-              onClick={() => {
-                setShowEducationTimeline(true);
-                setShowWorkTimeline(false);
-                setShowCertificationTimeline(false);
-              }}
-              className="mb-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-            >
-              Education
-            </button>
+          <main>
+        <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+        className='my-8'
+      >
+        <h1 className="text-7xl font-extrabold text-white text-center opacity-5 uppercase">
+        Qualification
+        </h1>
+        <h3 className="text-xl font-extrabold text-white text-center absolute top-6 left-[27%] vsm:left-[30%] m:left-[34%] md:left-[40%] lg:left-[43%] xl:left-[45%] ">
+        My personal journey
+        </h3>
+      </motion.div>
+        <ul className="flex justify-center relative bottom-6">
+      <li>
+        <button
+          onClick={() => setActiveButton('education')}
+          className={`mb-4 text-white py-2 px-4 flex gap-1 items-center ${activeButton === 'education' ? 'text-violet-600' : 'text-white'}`}
+        >
+          <Image
+        className={`${activeButton === 'education' ? 'inline' : 'hidden'}`}
+            src="/educationViolet.svg"
+            alt="education logo"
+            width={20}
+            height={20}
+          />
+            <Image
+        className={`${activeButton === 'education' ? 'hidden' : 'inline'}`}
+            src="/education.svg"
+            alt="education logo"
+            width={20}
+            height={20}
+          />
           
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                setShowEducationTimeline(false);
-                setShowWorkTimeline(true);
-                setShowCertificationTimeline(false);
-              }}
-              className="mb-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-            >
-              Work
-            </button>
           
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                setShowEducationTimeline(false);
-                setShowWorkTimeline(false);
-                setShowCertificationTimeline(true);
-              }}
-              className="mb-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-            >
-              Certifications
-            </button>
-          
-          </li>
-        </ul>
+          Education
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={() => setActiveButton('work')}
+          className={`mb-4 text-white py-2 px-4 flex gap-1 items-center ${activeButton === 'work' ? 'text-violet-600' : 'text-white'}`}
+        >
+          <Image
+        className={`${activeButton === 'work' ? 'inline' : 'hidden'}`}
+            src="/workViolet.svg"
+            alt="work logo"
+            width={20}
+            height={20}
+          />
+            <Image
+        className={`${activeButton === 'work' ? 'hidden' : 'inline'}`}
+            src="/work.svg"
+            alt="work logo"
+            width={20}
+            height={20}
+          />
+          Work
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={() => setActiveButton('certification')}
+          className={`mb-4 text-white py-2 px-4 flex gap-1 items-center ${activeButton === 'certification' ? 'text-violet-600' : 'text-white'}`}
+        >
+            <Image
+        className={`${activeButton === 'certification' ? 'inline' : 'hidden'}`}
+            src="/certificationViolet.svg"
+            alt="certification logo"
+            width={20}
+            height={20}
+          />
+            <Image
+        className={`${activeButton === 'certification' ? 'hidden' : 'inline'}`}
+            src="/certification.svg"
+            alt="certification logo"
+            width={20}
+            height={20}
+          />
+          Certifications
+        </button>
+      </li>
+    </ul>
         <div>
         {showEducationTimeline && <Timeline data={educationData} />}
         {showWorkTimeline && <Timeline data={workData} />}
